af686e24f7e722e0538c580bb0aa69054a1d4fd9
Matheus <matheusdg.2006@gmail.com>
2026-05-05 14:34:58 -0300

    iniciando projeto

---
+++ src/main/java/com/barbearia/projetob/ProjetobApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.projetob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbearia/projetob/ProjetobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.projetob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
